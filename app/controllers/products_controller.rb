0fe03a64ca5f3bc8caf1fafcb837c653e39af768
koiocya <73815463+koiocya@users.noreply.github.com>
2020-12-31 18:44:31 +0900

    search view編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,11 +14,15 @@ class ProductsController < ApplicationController
   end
 
   def search
+    @products = Product.all
+    if params[:keyword]
+      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword],sort: '+itemPrice',minPrice: params[:minPrice],NGKeyword: '中古',imageFlag: '1',itemId: 'alude:10179937')
+    end
   end
 
 
   private
-  
+
     def product_params
       params.require(:product).permit(:name, :price, :code)
     end
