078bd9e11af9e04dd2c9783337b9d2a7ef2a3534
koiocya <73815463+koiocya@users.noreply.github.com>
2020-12-31 18:22:13 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: 'products#search'
+
+  resources :products, only:[:new, :create] do
+    collection do
+      get 'search'
+    end
+  end
+
 end
