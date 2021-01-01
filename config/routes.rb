d9700423b7b216c9e88c973b4edb02a109360345
koiocya <73815463+koiocya@users.noreply.github.com>
2021-01-01 19:23:43 +0900

    削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root to: 'products#search'
 
-  resources :products, only:[:new, :create] do
+  resources :products, only:[:new, :create, :destroy] do
     collection do
       get 'search'
     end
